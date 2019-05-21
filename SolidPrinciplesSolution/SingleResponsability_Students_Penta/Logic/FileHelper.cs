58711a91612f8465c9cc812609e769a8c7efee74
nadiacomanici <nadi_comanici@yahoo.com>
2019-05-21 12:07:53 +0300

    Added Implementation during .Net Community

---
+++ SolidPrinciplesSolution/SingleResponsability_Students_Penta/Logic/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsability_Students_Penta.Logic
+{
+    public class FileHelper
+    {
+        public static string GetFullFilePath(string folderName, string fileName)
+        {
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), folderName);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            return Path.Combine(folder, fileName);
+        }
+    }
+}
+
+
